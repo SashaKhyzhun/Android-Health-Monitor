65e9a42a4987b45ff030a83159dcfa2b9f2171f0
SashaKhyzhun <sasha.khyzhun@gmail.com>
2018-11-03 02:15:01 +0200

    added bubble * bar charts

--- mobile/src/main/java/com/sashakhyzhun/healthmonitor/ui/analytics/AnalyticsFragment.kt
+++ mobile/src/main/java/com/sashakhyzhun/healthmonitor/ui/analytics/AnalyticsFragment.kt
@@ -6,17 +6,12 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 
-import com.github.mikephil.charting.charts.BarChart;
-import com.github.mikephil.charting.utils.ColorTemplate;
-
 import com.sashakhyzhun.healthmonitor.R
-import com.github.mikephil.charting.interfaces.datasets.IBarDataSet
-import com.sashakhyzhun.healthmonitor.R.id.chart
-import com.github.mikephil.charting.components.YAxis
-import com.github.mikephil.charting.components.AxisBase
 import com.github.mikephil.charting.formatter.IAxisValueFormatter
 import com.github.mikephil.charting.components.XAxis
 import android.support.v4.content.ContextCompat
+import com.github.mikephil.charting.charts.BarChart
+import com.github.mikephil.charting.charts.BubbleChart
 import com.github.mikephil.charting.charts.LineChart
 import com.github.mikephil.charting.data.*
 
@@ -30,8 +25,19 @@ class AnalyticsFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.analytics_fragment, container, false)
 
-        val chart: LineChart = view.findViewById(R.id.chart)
+        val lineChart: LineChart = view.findViewById(R.id.lineChart)
+        createLineChart(lineChart)
 
+        val bubbleChart: BubbleChart = view.findViewById(R.id.bubbleChart)
+        createBubbleChart(bubbleChart)
+
+        val barChart: BarChart = view.findViewById(R.id.barChart)
+        createBarChart(barChart)
+
+        return view
+    }
+
+    private fun createLineChart(chart: LineChart) {
         val entries = ArrayList<Entry>()
         entries.add(Entry(0F, 4F))
         entries.add(Entry(1F, 1F))
@@ -66,12 +72,92 @@ class AnalyticsFragment : Fragment() {
 
         // Setting Data
         val data = LineData(dataSet)
-        chart.setData(data)
+        chart.data = data
         chart.animateX(2500)
         //refresh
         chart.invalidate()
+    }
 
-        return view
+    private fun createBubbleChart(chart: BubbleChart) {
+        val entries = ArrayList<BubbleEntry>()
+        entries.add(BubbleEntry(1F, 1F, 1F))
+        entries.add(BubbleEntry(2F, 2F, 2F))
+        entries.add(BubbleEntry(3F, 3F, 3F))
+        entries.add(BubbleEntry(4F, 4F, 4F))
+
+        val dataSet = BubbleDataSet(entries, "Customized values")
+        dataSet.color = ContextCompat.getColor(context!!, R.color.colorPrimary)
+        dataSet.valueTextColor = ContextCompat.getColor(context!!, R.color.colorPrimaryDark)
+
+        //****
+        // Controlling X axis
+        val xAxis = chart.xAxis
+        // Set the xAxis position to bottom. Default is top
+        xAxis.position = XAxis.XAxisPosition.BOTTOM
+        //Customizing x axis value
+        val months = arrayOf("Jan", "Feb", "Mar", "Apr")
+
+        //val formatter = IAxisValueFormatter { value, axis -> months[value.toInt()] }
+        xAxis.granularity = 1f // minimum axis-step (interval) is 1
+        //xAxis.valueFormatter = formatter
+
+        //***
+        // Controlling right side of y axis
+        val yAxisRight = chart.axisRight
+        yAxisRight.isEnabled = false
+
+        //***
+        // Controlling left side of y axis
+        val yAxisLeft = chart.axisLeft
+        yAxisLeft.granularity = 1f
+
+        // Setting Data
+        val data = BubbleData(dataSet)
+        chart.data = data
+        chart.animateX(2500)
+        //refresh
+        chart.invalidate()
+    }
+
+    private fun createBarChart(chart: BarChart) {
+        val entries = ArrayList<BarEntry>()
+        entries.add(BarEntry(0F, 4F))
+        entries.add(BarEntry(1F, 1F))
+        entries.add(BarEntry(2F, 2F))
+        entries.add(BarEntry(3F, 4F))
+
+        val dataSet = BarDataSet(entries, "Customized values")
+        dataSet.color = ContextCompat.getColor(context!!, R.color.colorPrimary)
+        dataSet.valueTextColor = ContextCompat.getColor(context!!, R.color.colorPrimaryDark)
+
+        //****
+        // Controlling X axis
+        val xAxis = chart.xAxis
+        // Set the xAxis position to bottom. Default is top
+        xAxis.position = XAxis.XAxisPosition.BOTTOM
+        //Customizing x axis value
+        val months = arrayOf("Jan", "Feb", "Mar", "Apr")
+
+        val formatter = IAxisValueFormatter { value, axis -> months[value.toInt()] }
+        xAxis.granularity = 1f // minimum axis-step (interval) is 1
+        xAxis.valueFormatter = formatter
+
+        //***
+        // Controlling right side of y axis
+        val yAxisRight = chart.axisRight
+        yAxisRight.isEnabled = false
+
+        //***
+        // Controlling left side of y axis
+        val yAxisLeft = chart.axisLeft
+        yAxisLeft.granularity = 1f
+
+        // Setting Data
+        val data = BarData(dataSet)
+        chart.data = data
+        chart.animateX(2500)
+        //refresh
+        chart.invalidate()
     }
 
 
